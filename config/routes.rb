a33ba9804c72ef5d581d92a06f6c9570c5a64fba
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-13 12:09:30 -0400

    add navbar && pages w/ routes 🚜

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+ # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   root 'welcome#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'about', to: 'about#index'
+  get 'contact', to: 'contact#index'
+  get 'calendar', to: 'calendar#index'
 end
