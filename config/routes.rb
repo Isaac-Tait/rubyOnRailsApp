a99e3c39089551a0d9279b77a667a092bdc28cd1
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-16 13:22:26 -0400

    feat: add form 🥸

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  
   get 'about', to: 'about#index'
   get 'contact', to: 'contact#index'
   get 'calendar', to: 'calendar#index'
