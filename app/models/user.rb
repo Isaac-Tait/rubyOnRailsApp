ac3c9cea189576cf93f455fe04d2f33364d8b6c0
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-16 16:48:20 -0400

    feat: add password regex w/ placeholders 🥳

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+    validates :password, format: { with: /\A[([a-z]|[A-Z])0-9_-]{6,40}/ }
 end
