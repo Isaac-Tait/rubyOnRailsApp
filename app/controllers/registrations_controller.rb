2bb81c3b881daf8de6594ebd2cd4b6b02a24c62f
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-24 11:01:16 -0400

    feat: bypass hotwire breaking form (rails 7) 🚂

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,7 @@ class RegistrationsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to root_path, notice: "Thank you for signing up!"
         else
-            render :new
+            render :new, status: :unprocessable_entity
         end
     end
 
