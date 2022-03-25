a4ac0a6caa02ed7cb934b73b99f81d23bb8b9183
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-24 21:29:22 -0400

    feat: add log out button🪖

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+  delete 'log_out', to: 'sessions#destroy'
   
   get 'about', to: 'about#index'
   get 'contact', to: 'contact#index'
