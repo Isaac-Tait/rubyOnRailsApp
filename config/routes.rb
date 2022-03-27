3f77732d6d33621235dfd210d8c3b95d0918ca68
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-27 17:21:50 -0400

    chore: i am stuck on sign in page 🤦🏻‍♂️

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
   delete 'log_out', to: 'sessions#destroy'
   
   get 'about', to: 'about#index'
