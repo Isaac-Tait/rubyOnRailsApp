85358e107246b01a78d283fd0b7afb36abf4ec84
Isaac Tait <isaac@mountaintopcoding.com>
2022-04-01 11:34:29 -0400

    chore: sort out broken submit button 🥳

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,17 @@
 class SessionsController < ApplicationController 
     def new
-
     end
 
     def create
         user = User.find_by(email: params[:email])
         if user.present? && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect_to root_path, notice: "You have been logged in."
+            redirect_to root_path, notice: "Logged in successfully"
         else
-            flash.now.alert = "Invalid email or password."
-            render :new, status: :unprocessable_entity
+            flash[:alert] = "Invalid email or password"
+            render :new
         end
-    end 
+    end
 
     def destroy
         session[:user_id] = nil
